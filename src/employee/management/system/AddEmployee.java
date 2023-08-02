3060ba3f3cf909306cb831547ff0c80b80891749
Jachimike Divine Ezenwamadu <94161891+JachiEze@users.noreply.github.com>
2023-08-02 05:01:12 -0400

    Update AddEmployee.java

--- src/employee/management/system/AddEmployee.java
+++ src/employee/management/system/AddEmployee.java
@@ -11,9 +11,9 @@ public class AddEmployee extends JFrame implements ActionListener{
     Random ran = new Random();
     int number = ran.nextInt(999999);
     
-    JTextField tfname, tffname, tfaddress, tfphone, tfaadhar, tfemail, tfsalary, tfdesignation;
-    JDateChooser dcdob;
-    JComboBox cbeducation;
+    JTextField efname, elname, eaddress, ephone, epostalcode, eemail, esalary, edesignation;
+    JDateChooser edob;
+    JComboBox eeducation;
     JLabel lblempId;
     JButton add, back;
     
@@ -31,63 +31,63 @@ public class AddEmployee extends JFrame implements ActionListener{
         labelname.setFont(new Font("serif", Font.PLAIN, 20));
         add(labelname);
         
-        tfname = new JTextField();
-        tfname.setBounds(200, 150, 150, 30);
-        add(tfname);
+        efname = new JTextField();
+        efname.setBounds(200, 150, 150, 30);
+        add(efname);
         
         JLabel labelfname = new JLabel("Last Name");
         labelfname.setBounds(400, 150, 150, 30);
         labelfname.setFont(new Font("serif", Font.PLAIN, 20));
         add(labelfname);
         
-        tffname = new JTextField();
-        tffname.setBounds(600, 150, 150, 30);
-        add(tffname);
+        elname = new JTextField();
+        elname.setBounds(600, 150, 150, 30);
+        add(elname);
         
         JLabel labeldob = new JLabel("Date of Birth");
         labeldob.setBounds(50, 200, 150, 30);
         labeldob.setFont(new Font("serif", Font.PLAIN, 20));
         add(labeldob);
         
-        dcdob = new JDateChooser();
-        dcdob.setBounds(200, 200, 150, 30);
-        add(dcdob);
+        edob = new JDateChooser();
+        edob.setBounds(200, 200, 150, 30);
+        add(edob);
         
         JLabel labelsalary = new JLabel("Salary");
         labelsalary.setBounds(400, 200, 150, 30);
         labelsalary.setFont(new Font("serif", Font.PLAIN, 20));
         add(labelsalary);
         
-        tfsalary = new JTextField();
-        tfsalary.setBounds(600, 200, 150, 30);
-        add(tfsalary);
+        esalary = new JTextField();
+        esalary.setBounds(600, 200, 150, 30);
+        add(esalary);
         
         JLabel labeladdress = new JLabel("Address");
         labeladdress.setBounds(50, 250, 150, 30);
         labeladdress.setFont(new Font("serif", Font.PLAIN, 20));
         add(labeladdress);
         
-        tfaddress = new JTextField();
-        tfaddress.setBounds(200, 250, 150, 30);
-        add(tfaddress);
+        eaddress = new JTextField();
+        eaddress.setBounds(200, 250, 150, 30);
+        add(eaddress);
         
         JLabel labelphone = new JLabel("Phone");
         labelphone.setBounds(400, 250, 150, 30);
         labelphone.setFont(new Font("serif", Font.PLAIN, 20));
         add(labelphone);
         
-        tfphone = new JTextField();
-        tfphone.setBounds(600, 250, 150, 30);
-        add(tfphone);
+        ephone = new JTextField();
+        ephone.setBounds(600, 250, 150, 30);
+        add(ephone);
         
         JLabel labelemail = new JLabel("Email");
         labelemail.setBounds(50, 300, 150, 30);
         labelemail.setFont(new Font("serif", Font.PLAIN, 20));
         add(labelemail);
         
-        tfemail = new JTextField();
-        tfemail.setBounds(200, 300, 150, 30);
-        add(tfemail);
+        eemail = new JTextField();
+        eemail.setBounds(200, 300, 150, 30);
+        add(eemail);
         
         JLabel labeleducation = new JLabel("Higest Education");
         labeleducation.setBounds(400, 300, 150, 30);
@@ -95,28 +95,28 @@ public class AddEmployee extends JFrame implements ActionListener{
         add(labeleducation);
         
         String courses[] = {"BBA", "BCA", "BA", "BSC", "B.COM", "BTech", "MBA", "MCA", "MA", "MTech", "MSC", "PHD"};
-        cbeducation = new JComboBox(courses);
-        cbeducation.setBackground(Color.WHITE);
-        cbeducation.setBounds(600, 300, 150, 30);
-        add(cbeducation);
+        eeducation = new JComboBox(courses);
+        eeducation.setBackground(Color.WHITE);
+        eeducation.setBounds(600, 300, 150, 30);
+        add(eeducation);
         
         JLabel labeldesignation = new JLabel("Designation");
         labeldesignation.setBounds(50, 350, 150, 30);
         labeldesignation.setFont(new Font("serif", Font.PLAIN, 20));
         add(labeldesignation);
         
-        tfdesignation = new JTextField();
-        tfdesignation.setBounds(200, 350, 150, 30);
-        add(tfdesignation);
+        edesignation = new JTextField();
+        edesignation.setBounds(200, 350, 150, 30);
+        add(edesignation);
         
         JLabel labelaadhar = new JLabel("Postal Code");
         labelaadhar.setBounds(400, 350, 150, 30);
         labelaadhar.setFont(new Font("serif", Font.PLAIN, 20));
         add(labelaadhar);
         
-        tfaadhar = new JTextField();
-        tfaadhar.setBounds(600, 350, 150, 30);
-        add(tfaadhar);
+        epostalcode = new JTextField();
+        epostalcode.setBounds(600, 350, 150, 30);
+        add(epostalcode);
         
         JLabel labelempId = new JLabel("Employee id");
         labelempId.setBounds(50, 400, 150, 30);
@@ -149,21 +149,21 @@ public class AddEmployee extends JFrame implements ActionListener{
     
     public void actionPerformed(ActionEvent ae) {
         if (ae.getSource() == add) {
-            String name = tfname.getText();
-            String fname = tffname.getText();
-            String dob = ((JTextField) dcdob.getDateEditor().getUiComponent()).getText();
-            String salary = tfsalary.getText();
-            String address = tfaddress.getText();
-            String phone = tfphone.getText();
-            String email = tfemail.getText();
-            String education = (String) cbeducation.getSelectedItem();
-            String designation = tfdesignation.getText();
-            String aadhar = tfaadhar.getText();
+            String fname = efname.getText();
+            String lname = elname.getText();
+            String dob = ((JTextField) edob.getDateEditor().getUiComponent()).getText();
+            String salary = esalary.getText();
+            String address = eaddress.getText();
+            String phone = ephone.getText();
+            String email = eemail.getText();
+            String education = (String) eeducation.getSelectedItem();
+            String designation = edesignation.getText();
+            String postalcode = epostalcode.getText();
             String empId = lblempId.getText();
             
             try {
                 Conn conn = new Conn();
-                String query = "insert into employee values('"+name+"', '"+fname+"', '"+dob+"', '"+salary+"', '"+address+"', '"+phone+"', '"+email+"', '"+education+"', '"+designation+"', '"+aadhar+"', '"+empId+"')";
+                String query = "insert into employee values('"+fname+"', '"+lname+"', '"+dob+"', '"+salary+"', '"+address+"', '"+phone+"', '"+email+"', '"+education+"', '"+designation+"', '"+postalcode+"', '"+empId+"')";
                 conn.s.executeUpdate(query);
                 JOptionPane.showMessageDialog(null, "Details added successfully");
                 setVisible(false);
